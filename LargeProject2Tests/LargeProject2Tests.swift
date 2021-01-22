308b10e73e125b45476a75ccceb3633089dfe618
Gautam Mittal <gmittal@expedia.com>
2021-01-22 22:09:42 +0530

    added Test

--- LargeProject2Tests/LargeProject2Tests.swift
+++ LargeProject2Tests/LargeProject2Tests.swift
@@ -19,15 +19,7 @@ class LargeProject2Tests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let i = 0
+        XCTAssertTrue(i == 0)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
